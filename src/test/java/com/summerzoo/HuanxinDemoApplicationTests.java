8e6f6879788cc43e4054a630bfb2ed7535b473d3
ryan <625056163@qq.com>
2018-05-25 14:32:24 +0800

    second commit

---
+++ src/test/java/com/summerzoo/HuanxinDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.summerzoo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HuanxinDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
